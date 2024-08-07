c438faaa1c2537e997705a8254547ca112e5873f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-07 10:43:33 +0300

    ✨ feat(all_exams_screen): Add next exam list and loading state
    This commit adds the implementation of the next exam list feature to the
    `AllExams` screen. It includes:
    
    - Displaying the next exam list with pagination functionality
    - Adding a loading state indicator when fetching the next exam list
    - Handling the case when there are no items available in the next exam list
    
    The changes were made to enhance the user experience and provide a more
    comprehensive view of the available exams.

--- lib/screens/all_exams_screen.dart
+++ lib/screens/all_exams_screen.dart
@@ -1,9 +1,11 @@
 import 'package:control_proctor/resource_manager/index.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import '../controllers/all_exam_controller.dart';
 import '../resource_manager/ReusableWidget/drop_down_button.dart';
 import '../resource_manager/ReusableWidget/loading_indicators.dart';
+import 'widget/next_exam_widget.dart';
 import 'widget/side_menu.dart';
 
 class AllExams extends GetView<AllExamController> {
@@ -40,13 +42,6 @@ class AllExams extends GetView<AllExamController> {
                 );
               }
 
-              if (controller.optionsControlMission.isEmpty) {
-                return Text(
-                  'No items available',
-                  style: nunitoBoldStyle(),
-                );
-              }
-
               return SizedBox(
                 width: Get.width,
                 child: Padding(
@@ -54,10 +49,75 @@ class AllExams extends GetView<AllExamController> {
                   child: MultiSelectDropDownView(
                     hintText: "Select Control Mission",
                     onOptionSelected: (selectedItem) {
-                      //  controller.setSelectedItemControlMission(selectedItem);
+                      selectedItem.isNotEmpty
+                          ? controller.getExamMissionsByControlMissionId(
+                              controlMissionId: selectedItem.first.value)
+                          : controller.nextExamList.clear();
+
+                      controller.update();
                     },
                     options: controller.optionsControlMission,
                   ),
+                ),
+              );
+            },
+          ),
+          GetBuilder<AllExamController>(
+            builder: (controller) {
+              if (controller.isLoadingGetNextExam) {
+                return Expanded(
+                  child: Center(
+                    child: LoadingIndicators.getLoadingIndicator(),
+                  ),
+                );
+              }
+              return Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 10.0, left: 10, right: 10),
+                        child: controller.nextExamList.isEmpty
+                            ? Center(
+                                child: Text(
+                                  "No items available",
+                                  style: nunitoBoldStyle(),
+                                ),
+                              )
+                            : ListView.builder(
+                                itemCount: controller.nextExamList.length,
+                                itemBuilder: (context, index) {
+                                  var nextExamResModel =
+                                      controller.nextExamList[index];
+                                  if (nextExamResModel.examMissionsResModel
+                                          ?.data?.isEmpty ??
+                                      true) {
+                                    return Container();
+                                  }
+
+                                  return Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: List.generate(
+                                      nextExamResModel
+                                          .examMissionsResModel!.data!.length,
+                                      (i) {
+                                        var mission = nextExamResModel
+                                            .examMissionsResModel!.data![i];
+                                        return NextExamWidget(
+                                          nextExamResModel: nextExamResModel,
+                                          examMissionResModel: mission,
+                                          index: i,
+                                        );
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
+                      ),
+                    )
+                  ],
                 ),
               );
             },
