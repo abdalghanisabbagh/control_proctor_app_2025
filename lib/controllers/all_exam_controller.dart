32cf40232eb082f5e886816cb102877e5e707f7f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-07 10:43:28 +0300

    ✨ feat(AllExamController): Add functionality to fetch exam missions by control mission ID
    This commit adds a new method `getExamMissionsByControlMissionId` to the `AllExamController` class. This method is responsible for fetching the exam missions associated with a specific control mission ID. 
    
    The changes include:
    
    - Add a new method `getExamMissionsByControlMissionId` that makes a GET request to the `getControlMissionByProctor` endpoint with the provided `controlMissionId`.
    - Update the `onInit` method to call `getControlMissionsByProctorId` instead of `getControlMissionByproctor`.
    - Add new imports for the `NextExamsResModel` and `NextExamResModel` classes.
    - Update the `isLoadingGetNextExam` flag to track the loading state of the `getExamMissionsByControlMissionId` method.
    - Update the `nextExamList` field to store the fetched exam missions.
    
    These changes are made to enhance the functionality of the `AllExamController` class and provide the ability to fetch exam missions based on a specific control mission ID.

--- lib/controllers/all_exam_controller.dart
+++ lib/controllers/all_exam_controller.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:control_proctor/models/next%20exam/next_exams_res_model.dart';
 import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
 import 'package:multi_dropdown/models/value_item.dart';
@@ -7,14 +8,17 @@ import '../configurations/app_links.dart';
 import '../enums/req_type_enum.dart';
 import '../models/control_mission/control_missions_res_model.dart';
 import '../models/failure_model.dart';
+import '../models/next exam/next_exam_res_model.dart';
 import '../resource_manager/ReusableWidget/show_dialgue.dart';
 import '../tools/response_handler.dart';
 
 class AllExamController extends GetxController {
   bool isLoadingGetControlMission = false;
+  bool isLoadingGetNextExam = false;
   List<ValueItem> optionsControlMission = <ValueItem>[];
+  List<NextExamResModel> nextExamList = [];
 
-  Future<void> getControlMissionByproctor() async {
+  Future<void> getControlMissionsByProctorId() async {
     isLoadingGetControlMission = true;
 
     update();
@@ -46,9 +50,38 @@ class AllExamController extends GetxController {
     update();
   }
 
+  Future<void> getExamMissionsByControlMissionId(
+      {required int controlMissionId}) async {
+    isLoadingGetNextExam = true;
+
+    update();
+
+    ResponseHandler<NextExamsResModel> responseHandler = ResponseHandler();
+    Either<Failure, NextExamsResModel> response =
+        await responseHandler.getResponse(
+      path: '${ProctorsLinks.getControlMissionByProctor}/$controlMissionId',
+      converter: NextExamsResModel.fromJson,
+      type: ReqTypeEnum.GET,
+    );
+    response.fold(
+      (l) {
+        MyAwesomeDialogue(
+          title: 'Error',
+          desc: l.message,
+          dialogType: DialogType.error,
+        ).showDialogue(Get.key.currentContext!);
+      },
+      (r) {
+        nextExamList = r.data!;
+      },
+    );
+    isLoadingGetNextExam = false;
+    update();
+  }
+
   @override
   void onInit() {
-    getControlMissionByproctor();
+    getControlMissionsByProctorId();
 
     super.onInit();
   }
