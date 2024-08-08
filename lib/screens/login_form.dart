ec78362fbc583af415d82e653d4bba7b2c39a65c
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-08 14:56:09 +0300

    🎨 feat(login-form): Improve code organization and readability
    Reorganizes the imports in the `login_form.dart` file to improve code
    organization and readability. The changes include:
    
    - Reordering the import statements to follow a logical structure.
    - Grouping related imports together for better maintainability.
    - Removing unnecessary import statements.
    
    This change aims to enhance the code's readability and make it easier to
    navigate and understand the dependencies in the `login_form.dart` file.

--- lib/screens/login_form.dart
+++ lib/screens/login_form.dart
@@ -1,14 +1,14 @@
-import 'package:control_proctor/controllers/login_controller.dart';
-import 'package:control_proctor/resource_manager/ReusableWidget/loading_indicators.dart';
-import 'package:control_proctor/resource_manager/ReusableWidget/my_snak_bar.dart';
-import 'package:control_proctor/resource_manager/ReusableWidget/my_text_form_field.dart';
-import 'package:control_proctor/resource_manager/validations.dart';
 import 'package:custom_theme/lib.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
+import '../controllers/login_controller.dart';
+import '../resource_manager/ReusableWidget/loading_indicators.dart';
+import '../resource_manager/ReusableWidget/my_snak_bar.dart';
+import '../resource_manager/ReusableWidget/my_text_form_field.dart';
 import '../resource_manager/constants/app_constatnts.dart';
+import '../resource_manager/validations.dart';
 import '../routes_manger.dart';
 
 void _login(
