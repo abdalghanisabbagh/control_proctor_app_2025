2224a4ade593962fca84ee0105d285c83e914725
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-08 15:22:45 +0300

    ✨ feat: Restructure NextExamResModel properties
    Restructures the properties of the `NextExamResModel` class to
    improve the data model's organization and readability. The
    changes include:
    
    - Reordering the properties to follow a more logical sequence
    - Grouping related properties together for better clarity
    - Removing unnecessary whitespace and formatting the code
      for better consistency

--- lib/models/next exam/next_exam_res_model.dart
+++ lib/models/next exam/next_exam_res_model.dart
@@ -2,19 +2,19 @@ import '../exam mission/exam_missions_res_model.dart';
 import '../exam%20room/exam_room_res_model.dart';
 
 class NextExamResModel {
-  ExamRoomResModel? examRoomResModel;
-  String? month;
-  String? year;
-  bool? period;
   ExamMissionsResModel? examMissionsResModel;
 
+  ExamRoomResModel? examRoomResModel;
+
+  String? month;
+  bool? period;
+  String? year;
   NextExamResModel(
       {this.examRoomResModel,
       this.month,
       this.year,
       this.period,
       this.examMissionsResModel});
-
   NextExamResModel.fromJson(json) {
     examRoomResModel = json['exam_room'] != null
         ? ExamRoomResModel.fromJson(json['exam_room'])
