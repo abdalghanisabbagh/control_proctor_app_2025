d74cf59eb990fbba8a05e3119a83b622e6a34563
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 12:47:42 +0300

    Update profile_controller.dart

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -8,23 +8,22 @@ import '../models/login_response/user_profile_model.dart';
 class ProfileController extends GetxController {
   UserProfileModel? _cachedUserProfile;
 
-  // bool canAccessWidget({required String widgetId}) {
-  //   return (cachedUserProfile?.roles
-  //           ?.map((role) => role.screens
-  //               ?.map((screen) => screen.frontId)
-  //               .contains(widgetId))
-  //           .firstOrNull ??
-  //       false);
-  // }
-
   UserProfileModel? get cachedUserProfile =>
       _cachedUserProfile ?? getProfileFromHiveBox();
 
+  /// Deletes the profile from the hive box and resets the cached profile
+  /// to null.
   Future<void> deleteProfileFromHiveBox() async {
     _cachedUserProfile = null;
     await Hive.box('Profile').clear();
   }
 
+  /// Gets the user profile from the hive box.
+  ///
+  /// If the box contains key 'Profile', it will return the user profile
+  /// from the box. Otherwise, it will return null.
+  ///
+  /// The cached user profile is also reset to the returned value.
   UserProfileModel? getProfileFromHiveBox() {
     var data = Hive.box('Profile').get('Profile');
     _cachedUserProfile = Hive.box('Profile').containsKey("Profile")
@@ -33,6 +32,13 @@ class ProfileController extends GetxController {
     return _cachedUserProfile;
   }
 
+  /// Saves the given user profile to the hive box.
+  ///
+  /// The user profile is serialized to a json string and stored in the
+  /// 'Profile' box with the key 'Profile'.
+  ///
+  /// This method also calls [update] to notify the widgets that depend on
+  /// this controller to rebuild.
   void saveProfileToHiveBox(UserProfileModel cachedUserProfile) {
     update();
     Hive.box('Profile').put('Profile', jsonEncode(cachedUserProfile.toJson()));
